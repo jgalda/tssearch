57854a56004711c3a0ce50546a013ae615c085d3
jg <jangalda@wp.pl>
2014-05-25 23:50:08 +0200

    v2

--- yourtravel/include/Spot.h
+++ yourtravel/include/Spot.h
@@ -6,6 +6,8 @@
 
 #include <Transport.h>
 #include <City.h>
+#include <Schedule.h>
+
 
 using namespace std;
 class Spot
@@ -13,12 +15,15 @@ class Spot
     public:
 
         City city_to_visit;
-        int time_of_visit;
         Transport mean_of_transport;
+
+        int arrival_time;
+        int departure_time;
+        int time_of_travel;
 
         Spot();
         virtual ~Spot();
-
+        void choose_best_transport_to_leave(Schedule timetable);
 
     protected:
     private:
